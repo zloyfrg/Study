3788f73c8d37a9f5bcb26db79344bb898a279239
zloyfrg <kogep@bk.ru>
2024-04-24 22:16:11 +0500

    24-04-2024-00-15

--- Python03-page74-taskA.py
+++ Python03-page74-taskA.py
@@ -4,10 +4,10 @@
 import random
 
 # Генерируем случайную чётную длину массива length_list в диапазоне от 2 до 10
-rand_list = []
 length_list = random.randint(2, 10)
 
 # Заполняем массив rand_list случайными числами в диапазоне от 1 до 50
+rand_list = []
 while length_list:
     rand_list.append(random.randint(1, 50))
     length_list -= 1
