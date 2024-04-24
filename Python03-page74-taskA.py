55e5a0dcb042b951c3606432f5578ee14413bbd7
zloyfrg <kogep@bk.ru>
2024-04-24 08:53:04 +0500

    24-04-2024-08-42

--- Python03-page74-taskA.py
+++ Python03-page74-taskA.py
@@ -2,13 +2,20 @@
 Заполнить массив случайными числами и выполнить циклический сдвиг элементов массива вправо на 1 элемент
 """
 import random
+
+# Генерируем случайную чётную длину массива length_list в диапазоне от 2 до 10
 rand_list = []
-a = []
 length_list = random.randint(2, 10)
+
+# Заполняем массив rand_list случайными числами в диапазоне от 1 до 50
 while length_list:
-    rand_list.append(random.randint(2, 50))
+    rand_list.append(random.randint(1, 50))
     length_list -= 1
+
+# Выводим получившийся массив
 print(f'Случайный массив: \n{rand_list}')
+
+# Выполняем циклический сдвиг элементов массива вправо на 1 элемент
 print(f'Циклический сдвиг элементов массива:\n{rand_list[len(rand_list)-1:]+rand_list[:len(rand_list)-1]}')
 
 
