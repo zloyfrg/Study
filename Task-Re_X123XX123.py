298232de7b497308c9380f23887e21ab7ba39e65
zloyfrg <kogep@bk.ru>
2024-04-24 16:00:16 +0500

    24-04-2024-08-42

--- Task-Re_X123XX123.py
+++ Task-Re_X123XX123.py
@@ -1,3 +1,3 @@
 import re
 
-print(re.findall(r'[а-я].\d{2}[а-я]{2}\d{3}', '2.5^5!@-46 +1.6А234АА17422=0.000566!f(-0asd+5asd123asdl;fk', flags=re.IGNORECASE))
+print(re.findall(r'[а-я].\d{2}[а-я]{2}\d{3}', '2.5^5!@-46 +1.6Е234СТ17422=0.000566!f(-0asd+5asd123asdl;fk', flags=re.IGNORECASE))
