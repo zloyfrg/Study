58557fd295a545eca44fc8c3144d32f585bc87ee
zloyfrg <kogep@bk.ru>
2024-04-24 08:48:52 +0500

    24-04-2024-08-41

--- Python03-page74-taskB.py
+++ Python03-page74-taskB.py
@@ -6,16 +6,16 @@
 import random
 # Генерируем случайную чётную длину массива length_list
 rand_list = []
-a = []
-b = []
 length_list = random.randint(1, 5)*2
 
 # Заполняем массив rand_list случайными числами в диапазоне от 1 до 50
 while length_list:
     rand_list.append(random.randint(1, 50))
     length_list -= 1
+
 # Выводим получившийся массив
 print(f'Массив:\n{rand_list}')
+
 # Разворачиваем половинки и выводим результирующий массив
 a = rand_list[:int(len(rand_list)/2)]
 b = rand_list[int(len(rand_list)/2):]
