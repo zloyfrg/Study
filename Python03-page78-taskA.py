f20821fbc6214d082ee7178e29d238b9ab76cc74
zloyfrg <kogep@bk.ru>
2024-04-24 08:46:43 +0500

    24-04-2024-08-40

--- Python03-page78-taskA.py
+++ Python03-page78-taskA.py
@@ -6,8 +6,6 @@
 import random
 # Генерируем случайную длину массива length_list длиной в диапазоне от 2 до 10
 rand_list = []
-a = []
-b = []
 length_list = random.randint(2, 10)
 
 # Заполняем массив rand_list случайными числами в диапазоне от -10 до 10
@@ -18,7 +16,8 @@ while length_list:
 print(f'Массив:\n{rand_list}')
 
 # Отбираем отрицательные чётные элементы в новый массив и выводим результат
+result_list = []
 for x in rand_list:
   if (x % 2 == 0) and (x < 0):
-    b.append(x)
-print(b)
+    result_list.append(x)
+print(result_list)
